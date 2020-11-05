05004cd44ab3a2130e65329459e712ac2d0a4730
hmccain <hmccain@vetsfirstchoice.com>
2020-11-05 13:28:44 -0700

    Return picked json.

--- app.ts
+++ app.ts
@@ -1,4 +1,7 @@
 const initializeIntl = require('./intl.ts')
+const _ = require('lodash')
+const en = require('./translations/1/en')
+const fr = require('./translations/1/fr')
 
 const express = require("express")
 
@@ -11,11 +14,41 @@ const getTranslations = async (appId, lang, body) => {
     return await initializeIntl(body)
 }
 
+const getJSON = (lang, body) => {
+    let json;
+
+    // replace with call to S3 or other external storage
+    switch (lang) {
+        case "fr":
+            json = fr;
+            break;
+        case "en":
+        default:
+            json = en;
+            break;
+    }
+
+    return _.pick(json, body);
+}
+
 app.use(express.json())
 
 app.get("/", (req, res) => {
     res.send("hello world")
 })
+
+app.get("/json/:lang", (req, res) => {
+    const { lang } = req.params
+    const { body } = req
+
+    if (!lang) {
+        res.status(404).send()
+    }
+
+    const picked = getJSON(lang, body)
+    res.status(200).send(res.json(picked))
+})
+
 
 app.get("/translations/:appId/:lang", async (req, res) => {
     const { appId, lang } = req.params
