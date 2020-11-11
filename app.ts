a4add241a73bcb56852f37ac5f15567e55c8ba7b
hmccain <hmccain@vetsfirstchoice.com>
2020-11-11 13:29:50 -0700

    Clean up server code and old i18n files.

--- app.ts
+++ app.ts
@@ -8,12 +8,6 @@ const express = require("express")
 const app = express()
 const port = 8000
 
-const getTranslations = async (appId, lang, body) => {
-    // do i18n stuff
-    console.log({ appId, lang, body })
-    return await initializeIntl(body)
-}
-
 const getJSON = (appId, lang) => {
     switch (lang) {
         case "fr":
@@ -32,10 +26,6 @@ app.use((_, res, next) => {
 
 app.use(express.json())
 
-app.get("/", (req, res) => {
-    res.send("hello world")
-})
-
 app.get("/json/:appId/:lang", (req, res) => {
     const { appId, lang } = req.params
 
@@ -43,34 +33,9 @@ app.get("/json/:appId/:lang", (req, res) => {
         res.status(404).send()
     }
 
-    const picked = getJSON(appId, lang)
-    console.log({ req, lang, picked })
-    res.status(200).send(res.json({ demo: picked }))
+    res.status(200).send({ "demo": getJSON(appId, lang)})
 })
 
-// app.get("/locale", (req, res) => {
-//     const { appId, lang } = req.params
-//
-//     if (!lang) {
-//         res.status(404).send()
-//     }
-//
-//     const picked = getJSON(appId, lang)
-//     console.log({ req, lang, picked })
-//     res.status(200).send(res.json({ demo: picked }))
-// })
-
-app.get("/translations/:appId/:lang", async (req, res) => {
-    const { appId, lang } = req.params
-    const { body } = req
-
-    if (!appId || !lang) {
-        res.status(404).send()
-    }
-
-    const translations = await getTranslations(appId, lang, body)
-    res.status(200).send(res.json(translations))
-})
 
 app.listen(port, () => {
     console.log(`i18n server listening on port ${port}`)
